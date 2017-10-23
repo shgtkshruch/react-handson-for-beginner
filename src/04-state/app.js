e72c7042cc6eacffab5674ca626709a576a53fcb
shgtkshruch <shgtk.shruch@gmail.com>
2017-10-23 19:40:23 +0900

    Lesson 4

--- src/04-state/app.js
+++ src/04-state/app.js
@@ -4,12 +4,17 @@ import ReactDOM from 'react-dom';
 const container = document.querySelector('.js-app');
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: 'Hello'};
+  }
+
   render() {
     return (
       <div>
         {/* inputの値とp要素の値が同期させるようにしてみましょう */}
-        <input type="text" value="Hello" onChange={() => {}} />
-        <p>Hello</p>
+        <input type="text" value={this.state.text} onChange={(e) => { this.setState({text: e.target.value}) }} />
+        <p>{this.state.text}</p>
       </div>
     )
   }
