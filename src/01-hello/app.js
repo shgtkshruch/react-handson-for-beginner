e8d60c27ad172cf3c2ea8600473e1a4154d83568
shgtkshruch <shgtk.shruch@gmail.com>
2017-10-23 19:22:20 +0900

    lesson 1

--- src/01-hello/app.js
+++ src/01-hello/app.js
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom';
 
 const container = document.querySelector('.js-app');
 
+ReactDOM.render(
+  <h1 style={{ color: 'green' }}>Hello, world!</h1>,
+  container
+);
+
 // ここに「Hello World」という文字を緑色で表示してみましょう
