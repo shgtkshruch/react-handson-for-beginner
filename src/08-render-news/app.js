68f0a5e72e4769509ce0b6831d7dad71d1c58217
shgtkshruch <shgtk.shruch@gmail.com>
2017-10-23 21:00:54 +0900

    Lesson 8

--- src/08-render-news/app.js
+++ src/08-render-news/app.js
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom';
 // サーバーからデータを取得するための関数
 import {fetchHackerNews} from '../api';
 
+import News from './news';
+
 const container = document.querySelector('.js-app');
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { items: [] };
+  }
+
+  componentDidMount() {
+    fetchHackerNews().then((json) => this.setState({ items: json }));
+  }
   render() {
     return (
       <div>
         {/* サーバーからデータを取得したあとは、NewsのComponentを表示して、その中でNewsItemのComponentを表示してみましょう */}
+        <News items={ this.state.items }/>
       </div>
     )
   }
